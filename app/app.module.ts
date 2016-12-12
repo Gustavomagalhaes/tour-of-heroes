00a9e306b2820ad840377f895fcd13313bcfaf95
Gustavo Magalhães <itsgustavopereira@gmail.com>
2016-12-12 16:40:20 -0300

    Aula 4 e 5

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,44 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule }  from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent }  from './hero-detail.component';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ 
+  imports: [ 
     BrowserModule, 
-    FormsModule 
+    FormsModule,
+		RouterModule.forRoot([
+			{
+				path: '',
+				redirectTo: '/dashboard',
+				pathMatch: 'full'
+			},
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			},
+			{
+				path: 'heroes',
+				component: HeroesComponent
+			}
+		]) 
   ],
   declarations: [ 
     AppComponent,
+    HeroesComponent,
+		DashboardComponent,
     HeroDetailComponent 
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
